32a201e19091fc0e8e63805e7d440e1ddea0d56b
vxXJordanXxv <JordantheGhost@protonmail.com>
2023-03-24 22:20:15 -0600

    Added the NFA class and the header comments

---
+++ p2Files/fa/nfa/NFA.java
@@ -0,0 +1,12 @@
+/*
+ * @author: Jordan Casper
+ * @author: Anton Leslie
+ * CS 361
+ * Date: 24 March 2023
+ * Description: Project 2: NFA Class
+ *
+ */
+package fa.nfa;
+
+public class NFA {
+}
--- p2Files/fa/nfa/NFAState.java
+++ p2Files/fa/nfa/NFAState.java
@@ -1,3 +1,11 @@
+/*
+ * @author: Jordan Casper
+ * @author: Anton Leslie
+ * CS 361
+ * Date: 24 March 2023
+ * Description: Project 2: NFAState Class
+ *
+ */
 package fa.nfa;
 
 import java.util.HashMap;
