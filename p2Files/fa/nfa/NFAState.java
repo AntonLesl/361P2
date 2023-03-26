cfd96e47a7be8119ba80f32759ddd563fd8ec335
vxXJordanXxv <JordantheGhost@protonmail.com>
2023-03-25 22:25:36 -0600

    Completed the NFAState class with some additional methods as well as some changes to existing methods which I left comments on.

--- p2Files/fa/nfa/NFAState.java
+++ p2Files/fa/nfa/NFAState.java
@@ -14,39 +14,91 @@ import java.util.Map;
 import java.util.Set;
 import fa.State;
 
+//I will be cleaning up these comments later of the changes that I made but
+//I am keeping them in for now before we submit the project so that
+//you know why I made the changes that I made.
 public class NFAState extends State {
     //set NFAState instance variables to private 3/24/2023 10:30 PM
     private String from;
     private char OnSymb;
-    //isFinal needs to be set through a public method 3/24/2023 10:30 PM
-    private boolean isFinal;
+    private boolean isFinal; //isFinal needs to be set through a public method 3/24/2023 10:30 PM
     private HashMap<String, NFAState> delta;
     // Map to model the state's transitions
-    private Map<Character, Set<NFAState>> transitions;
 
+    private HashMap<Character, HashSet<NFAState>> transitions; //Changed Map to Hashmap simply for consistency 3/25/2023 ~9:45 PM Commit
+    //Changed Set<NFAState> to HashSet<NFAState> as a Hashset will check for duplicates automatically
+    // as well as the Hashset having faster access time and therefor more efficient. ~10:00 PM 3/25/2023 Commit
     public NFAState(String name) {
         super(name);
-        transitions = new HashMap<Character, Set<NFAState>>();
+        isFinal = false;
+        transitions = new HashMap<Character, HashSet<NFAState>>();
     }
 
     /**
+     * @aythor Jordan Casper
+     * Public setter for the isFinal variable.
+     * @param b - True or False
+     */
+    public void setFinal(boolean b) {
+        this.isFinal = b;
+    }
+
+    /**
+     * @author Jordan Casper
+     * Getter to check whether the state is a final state or not.
+     * @return isFinal
+     */
+    public boolean getFinal() {
+        return isFinal;
+    }
+
+    /**
+     * @author Jordan Casper
+     * Getter to retrieve the transition functions.
+     * @return transitions HashMap
+     */
+    public HashMap<Character, HashSet<NFAState>> getTransitions() {
+        return transitions;
+    }
+
+    /**
+     * @author Anton Leslie
+     * @author Jordan Casper
      * Adds a transition to the state
      * @param onSymb the symbol triggering the transition
      * @param toState the state to transition to
      */
     public void addTransition(char onSymb, NFAState toState) {
-        Set<NFAState> states = transitions.getOrDefault(onSymb, new HashSet<NFAState>());
-        states.add(toState);
-        transitions.put(onSymb, states);
+        //Changed the addTransition method to check if there is already
+        // a HashSet that is associated with the onSymb and if there is not
+        // it creates a new empty Hashset. Then it adds the destination state(toState)
+        // to the HashSet and puts the updated HashSet back into the HashMap for
+        // the input symbol OnSymb. ~10:00 PM 3/25/2023 Commit
+        HashSet<NFAState> state = transitions.get(onSymb);
+        if (state == null) {
+            state = new HashSet<NFAState>();
+        }
+         state.add(toState);
+         transitions.put(onSymb, state);
     }
 
     /**
+     * @author Anton Leslie
+     * @author Jordan Casper
      * Returns the set of states reachable from this state on the given symbol
      * @param onSymb the symbol triggering the transition
      * @return set of reachable states, or empty set if no transition on the symbol
      */
     public Set<NFAState> getToStates(char onSymb) {
-        return transitions.getOrDefault(onSymb, new HashSet<NFAState>());
+        //Added error handling in case the return is null.
+        //as  ~10:00 PM 3/25/2023 Commit
+        HashSet<NFAState> temp = transitions.get(onSymb);
+        if(temp == null) {
+            System.err.println("Error: Returns null on " + onSymb + " from " + getName());
+            System.exit(1);
+        }
+        //If the return is not null then returns the ToStates.
+        return transitions.get(onSymb);
     }
 
 }
